2d9681e95027b79c8ff8bab9de950d1e628ba197
dzinobile <dzinobile@gmail.com>
2026-03-21 09:04:25 -0400

    created enpm701_hardware package for running on actual raspberry pi hardware

---
+++ enpm701_hardware/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'enpm701_hardware'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name, ['package.xml']))
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Daniel Zinobile',
+    maintainer_email='dzinobile@gmail.com',
+    description='Hardware interface package for ENPM701 robot',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'colorpicker_node = enpm701_hardware.colorpicker_node:main',
+        ],
+    },
+)
